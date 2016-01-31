687ac22863a86e10e00cf75a4a70d381baa797da
Andrew Hayward <alc.hayward@gmail.com>
2016-02-01 10:04:19 +1300

    sdfds

--- python/ranking.py
+++ python/ranking.py
@@ -258,7 +258,7 @@ class Seeding(Session,double_model):
     def get_teams_to_seed(self,round):
         neg_list = []   
         for g in self.games:
-            if 1 == 1:
+            if g.round >= round:
                 neg_list.append([t.id for t in g.teams])
         edge_list = []
         for ii in range(self.n_teams):
