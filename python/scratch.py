33ca6bdca9a4ee9853cc4b63d786520666e3a534
Andrew Hayward <alc.hayward@gmail.com>
2016-09-15 09:54:44 +1000

    started writing new kl code

--- python/scratch.py
+++ python/scratch.py
@@ -10,7 +10,7 @@ def shannon_entropy(prb_dist):
         prb_dist is a one dimensional numpy array of probabilities.
     """
     x = prb_dist[ prb_dist > 0.0] #filter out zero values
-    return np.sum(x*np.log(x))
+    return -np.sum(x*np.log(x))
 
 
 def bin_samples(sample):
@@ -22,13 +22,17 @@ def generate_samples(x, f_draw, n_samples=1):
     """draw samples from a distribution from each instance of x
         each row of x should contain the paramters of f_draw
         each sample should be an object comparable by == """
-    return sum([map(f_draw, x) for i in range(n_samples)])
+    return np.ndarray(sum([map(f_draw, x) for i in range(n_samples)]))
     # Gotta check that f_draw will be changing each time I hit it.
 
 
-def draw_from_possion():
+def draw_from_possion(x, rate_fn):
+    return np.ndarray(map(np.random.poisson,rate_fn(x)))
 
-    return numpy.random.poisson(lambda)
 
-def kl_info():
-
+def kl_info(trace, f_draw, team_inds, param_inds):
+    inds = team_inds + param_inds
+    return shannon_entropy(
+            bin_samples(
+                generate_samples(
+                    trace[inds][:],f_draw))))
