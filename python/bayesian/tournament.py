75b1f1882a889e94a3e2852407951a2fe95ac737
Andrew Hayward <alc.hayward@gmail.com>
2016-09-15 11:44:25 +1000

    restructing data structures by remving round from game

--- python/bayesian/tournament.py
+++ python/bayesian/tournament.py
@@ -1,16 +1,22 @@
 class Game:
 
     def __init__(self, teams, scores=None, game_id=None):
+        """Game object.
+            teams is a list of two teams. identifcation via object id
+            scores is a dictionary {team:score}
+            """
         self.id = game_id
         self.teams = teams
 
         if not scores:
             self.completed = False
-            self.scores = {zip(teams,[None, None])}
+            self.scores = {zip(teams, [None, None])}
         else:
             self.completed = True
             self.scores = scores
 
 
+# This function might be superflous at the moment. but I might want to use a more
+# sophistcated id tracking thing later
 def new_game(team_1, team_2):
     return Game([team_1, team_2])
