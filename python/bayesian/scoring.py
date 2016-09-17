ce69ccd50da45946c8764a87805adaff52258de4
Andrew Hayward <alc.hayward@gmail.com>
2016-09-17 10:15:33 +1000

    Fixed some more axis stuff

---
+++ python/bayesian/scoring.py
@@ -0,0 +1,22 @@
+# ways for scoring based off results
+import numpy as np
+from prob import expectation
+
+def wld(games):
+    pass
+
+
+def most_likely_ordering(games):
+    pass
+
+
+def average_strength(strength_trace):
+    return expectation(strength_trace, lambda x: x)
+
+
+def ordering(strengths):
+    return np.argsort(strengths, axis=1) + 1
+
+
+def average_ordering(strength_trace):
+    return expectation(strength_trace, ordering)
