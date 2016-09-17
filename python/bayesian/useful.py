eb83a7edd8355417be4cb4e642f54343518a7888
Andrew Hayward <alc.hayward@gmail.com>
2016-09-17 13:49:27 +1000

    brought in parallel proccessing stuff

--- python/bayesian/useful.py
+++ python/bayesian/useful.py
@@ -1,3 +1,6 @@
+import multiprocessing
+
+
 def unique_pairs(l, equ_fn=(lambda x, y: x == y)):
     # type: (iterable, (object, object) -> bool ) -> list
 
@@ -11,3 +14,30 @@ def unique_pairs(l, equ_fn=(lambda x, y: x == y)):
         if not any(map(lambda x: pair_cmp(x, p), pairs)):
             pairs.append(p)
     return pairs
+
+
+def parfun(f,q_in,q_out):
+    while True:
+        i,x = q_in.get()
+        if i is None:
+            break
+        q_out.put((i,f(x)))
+
+
+def parmap(f, X, nprocs = multiprocessing.cpu_count()):
+    q_in   = multiprocessing.Queue(1)
+    q_out  = multiprocessing.Queue()
+
+    proc = [multiprocessing.Process(target=parfun,args=(f,q_in,q_out)) for _ in range(nprocs)]
+    for p in proc:
+        p.daemon = True
+        p.start()
+
+    sent = [q_in.put((i,x)) for i,x in enumerate(X)]
+    [q_in.put((None,None)) for _ in range(nprocs)]
+    res = [q_out.get() for _ in range(len(sent))]
+
+    [p.join() for p in proc]
+
+    return [x for i,x in sorted(res)]
+
